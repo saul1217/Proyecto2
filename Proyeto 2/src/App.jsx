8ac4b998e9885d1e038eb34666df89cc654c2b1b
saul1217 <a1125250007@utch.edu.mx>
2026-03-09 18:46:42 -0600

    se agreaga el api
    no lo visual unicamente se ve en console

--- Proyeto 2/src/App.jsx
+++ Proyeto 2/src/App.jsx
@@ -8,11 +8,21 @@ function App() {
   const [valorInput, setValorInput] = useState('hola');
   const [gifs, setGifs] = useState();
 
-  
+
   const onChange = (evento) => {
     const valor = evento.target.value;
     setValorInput(valor);
   }
+
+  const getGifs = async () => {
+    const url = 'https://api.giphy.com/v1/gifs/search?api_key=puoAHOcYjckOvMJ5uPn5AoqrtNEpcREX&q=';
+    const respone = await fetch(url);
+    const data = await respone.json();
+    console.log(data);
+  }
+
+
+  getGifs();
 
   return (
   <>  

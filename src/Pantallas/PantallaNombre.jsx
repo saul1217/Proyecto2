fa16cc61866ac4bfee99560619d51da3f32d9661
saul1217 <a1125250007@utch.edu.mx>
2026-03-13 11:52:58 -0600

    se añaden las palabras clave de la busqueda

--- src/Pantallas/PantallaNombre.jsx
+++ src/Pantallas/PantallaNombre.jsx
@@ -1,8 +1,15 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
+const Palabras = [
+    "gay", "homosexual", "crazy", "handsome", "chris bumstead", "toji fus", "lloron",
+    "party", "nerd", "cool", "trans"];
+
 export const PantallaNombre = () => {
 
     return(
         <>
-        <div className="flex flex-col items-center justify-center min-h-screen text-white">
+        <div className="flex flex-col items-center justify-center min-h-screen text-white bg-gradient-to-r from-violet-800 to-slate-600">
             <h2 className="text-3xl mb-6">Ingresa tu nombre</h2>
             <form action="" className="flex flex-col items-center gap-8">
                 <input className="bg-white rounded-xl text-black text-center" type="text" placeholder="     Ingresa tu nombre"/>

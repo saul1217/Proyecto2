0d985f40fe5fca9c1dfa7175fa6d85c1d0cc1a5d
saul1217 <a1125250007@utch.edu.mx>
2026-03-13 15:54:59 -0600

    se termina de editar el css
    eso creo

--- src/Pantallas/PantallaInicio.jsx
+++ src/Pantallas/PantallaInicio.jsx
@@ -8,7 +8,7 @@ export const PantallaInicio = () =>{
         <>
         <div className="flex flex-col items-center justify-center min-h-screen text-white bg-gradient-to-r from-violet-800 to-slate-600">
             <h1 className="text-3xl ]">Descubre tu gif</h1>
-            <button onClick={() => navigate('Ingresa_nombre')} className="bg-blue-700 text-white transition-all rounded-full text-xl p-2 m-9">
+            <button onClick={() => navigate('Ingresa_nombre')} className="bg-blue-700 text-white hover:bg-blue-900 rounded-full text-xl p-2 m-9">
                 Descubre el gif que te define segun tu nombre 
             </button>
         </div>
--- src/Pantallas/PantallaResultado.jsx
+++ src/Pantallas/PantallaResultado.jsx
@@ -9,27 +9,28 @@ export const PantallaResultado = () => {
   return(
     <>
     <div className="min-h-screen w-full flex items-center justify-center bg-gradient-to-r from-violet-800 to-slate-600 text-white p-4">
-      <div  className="flex flex-col items-center w-full max-w-5xl">
+      <div className="flex flex-col items-center w-full max-w-6xl">
       <h1 className="text-4xl text-whitemb-20 font-black p-8 rounded-full">Este es el gif que más te define {nombre}</h1>
-      <div className="contenedor-resultado-gifs">
-        
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 md:gap-6 w-full max-w-sm md:max-w-5xl mb-10 px-2 " >
+
         {gifAleatorio && gifAleatorio.length > 0 ? (
           gifAleatorio.map((gif) => (
             <img 
             key={gif.id} 
             src={gif.images.original.url} 
             alt={gif.title} 
-            className="w-full aspect-square object-cover rounded-xl shadow-lg transition-transform hover:scale-105 "/>
+            className="w-full aspect-square object-cover   hover:scale-105" 
+                />
           ))
         ) : (
           <p className="text-xl mb-8 text-white"> Buscando tu gif...</p>
         ) }
-      </div>
+        </div>
 
         <div className="flex flex-col items-center text-center gap-4">
-          <p className="text-3xl text-white mt-20 mb-10 font-black p-2 rounded-full " >No te gusto el gif ? puedes buscar más</p>
-          <button onClick={() => navigate('/buscador')} className="bg-purple-600 hover:bg-purple-700 text-white text-4xl p-8 rounded-full transition-all">Buscar mas gifs</button>
-        </div>
+          <p className="text-3xl text-white  mb-10 font-black p-2 rounded-full " >No te gusto el gif ? puedes buscar más</p>
+          <button onClick={() => navigate('/buscador')} className="bg-purple-600 hover:bg-purple-700 text-white text-4xl p-4 rounded-full">Buscar mas gifs</button>
+      </div>
         </div>
       </div>
     </>

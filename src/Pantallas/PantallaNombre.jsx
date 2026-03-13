de1ed9157435b9c64bfc59246b0382c9581bcb52
saul1217 <a1125250007@utch.edu.mx>
2026-03-13 12:04:29 -0600

    se agrega la logica de PantallaNombre

--- src/Pantallas/PantallaNombre.jsx
+++ src/Pantallas/PantallaNombre.jsx
@@ -1,12 +1,32 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 
-const Palabras = [
+const palabras = [
     "gay", "homosexual", "crazy", "handsome", "chris bumstead", "toji fus", "lloron",
     "party", "nerd", "cool", "trans"];
 
 export const PantallaNombre = () => {
+    const [nombre, setNombre] = useState('');
+    const navigate = useNavigate();
 
+    const manejarSubmit = async (e) =>{
+        e.preventDefault();
+        if(nombre.trim === '') return;
+
+        const palabrasRandom = palabras[Math.floor(Math.random() * palabras.length)];
+        const url =`https://api.giphy.com/v1/gifs/search?api_key=puoAHOcYjckOvMJ5uPn5AoqrtNEpcREX&q=${palabrasRandom}&limit=1`;
+
+
+        try{
+            const resp = await fetch(url);
+            const {data} = await resp.json();
+            const gifData = data.length > 0 ? data[0] : null;
+
+            navigate('resultado', {state: {nombre: nombre, gifAleatorio: gifData}});
+        }catch (error){
+            console.error("error al buscar el gif", error)
+        }
+    }
     return(
         <>
         <div className="flex flex-col items-center justify-center min-h-screen text-white bg-gradient-to-r from-violet-800 to-slate-600">

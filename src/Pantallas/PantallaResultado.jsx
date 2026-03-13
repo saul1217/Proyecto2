bcf83d8b763923d29ad7a3472d8af66e015c65ab
saul1217 <a1125250007@utch.edu.mx>
2026-03-13 12:27:00 -0600

    se agrega la visualizacion del resultado del gif

--- src/Pantallas/PantallaResultado.jsx
+++ src/Pantallas/PantallaResultado.jsx
@@ -1,13 +1,16 @@
+import { useLocation } from "react-router-dom";
 
 export const PantallaResultado = () => {
-  
+  const location = useLocation();
+  const {nombre, gifAleatorio} = location.state || {};
 
 
   return(
     <>
     <div>
-      <h1>Este es el gif que mas se acerca a tu personalidad "nombre"</h1>
-      <img src="" />   </div>
+      <h1 className="text-white">Este es el gif que mas se acerca a tu personalidad {nombre}</h1>
+      <img src={gifAleatorio.images.original.url} alt={gifAleatorio.title} /> 
+      </div>
     </>
   )
 }
